312dc5c963ed8516f83838ca7981da7b86c3d892
tankmkni <62043937+tankmkni@users.noreply.github.com>
2020-10-27 18:37:34 +0900

    clock location code hasn to search

--- contents/clock/js/select_image.js
+++ contents/clock/js/select_image.js
@@ -1,5 +1,5 @@
 function setImg(){
-	var urlHash = location.hash.substring(1);
-	var fileSrc = "../img/" + urlHash + ".png";
+	var filename = location.search.split("=")[1];
+	var fileSrc = "../img/" + filename + ".png";
 	document.getElementById("secondMark").setAttribute( "src" , fileSrc );
 }
