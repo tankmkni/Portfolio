39a171304454a52bb8704324175870ec29ed21df
tankmkni <62043937+tankmkni@users.noreply.github.com>
2021-01-07 23:27:48 +0900

    add get_height to notebook

--- contents/notebook/js/common.js
+++ contents/notebook/js/common.js
@@ -1,8 +1,22 @@
-function setHeight( tag ){	/* 引数「tag」の内包要素の高さを取得し、「tag」の高さに指定する */
-	tag.style.height = tag.contentWindow.document.body.scrollHeight + "px";
+/* 引数「tag」の内包要素の高さを取得し、「tag」の高さに指定する */
+/* 2021.1.6 border/margin/padding幅も含めるように変更*/
+function setHeight( tag ){
+	// 引数「tag」内の要素のbodyタグを取得
+	let bodyTag = tag.contentWindow.document.body;
+	
+	// bodyタグのborder/paddingを含む高さを取得
+	let bodyHeight = bodyTag.offsetHeight;
+	
+	// bodyタグのマージンの値を取得して追加（上下にあるので×２）
+	bodyHeight += parseInt( getComputedStyle( bodyTag ).margin ) * 2;
+	
+	// 引数「tag」の高さをセット
+	tag.style.height = bodyHeight + "px";
 }
 
-function setIframesHeight(){	/* 複数のiframeの高さをセットする */
+
+/* 複数のiframeの高さをセットする */
+function setIframesHeight(){
 	var iframes = document.getElementsByTagName("iframe");
 
 	for( i = 0 ; i < iframes.length ; i++ ){
